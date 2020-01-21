af0a7e9bdb08b1ffef16f49912eec3a2db200260
Mr-houzi <1195742779@qq.com>
2020-01-21 17:34:17 +0800

    '适配Laravel'

---
+++ src/ServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace MrHouzi\Weather;
+
+
+class ServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+    // 延迟注册 它不会在框架启动就注册，而是当你调用到它的时候才会注册。
+    protected $defer = true;
+
+    public function register()
+    {
+        $this->app->singleton(Weather::class, function(){
+            return new Weather(config('services.weather.key'));
+        });
+
+        $this->app->alias(Weather::class, 'weather');
+    }
+
+    public function provides()
+    {
+        return [Weather::class, 'weather'];
+    }
+}
